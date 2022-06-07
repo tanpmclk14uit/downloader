dfcc2c31aeed3e279f6893e6500579cbe3fafc12
tanpmcluitk14 <tan.lk16.cla@gmail.com>
2022-06-07 10:11:09 +0700

    config no storyboard

--- downloader/ViewController.swift
+++ downloader/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var helloWorld: UILabel = {
+        let lable = UILabel()
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        lable.text = "Hello world"
+        lable.textColor = .black
+        lable.font = UIFont.systemFont(ofSize: 20)
+        return lable
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(helloWorld)
+        helloWorld.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        helloWorld.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         // Do any additional setup after loading the view.
     }
-
-
 }
 
