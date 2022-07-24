8716603e98bed1120f264ff191e6fce586f677c6
tanpmcluitk14 <tan.lk16.cla@gmail.com>
2022-07-25 01:19:02 +0700

    hopefullllllllll

--- downloader/AppDelegate.swift
+++ downloader/AppDelegate.swift
@@ -41,12 +41,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     lazy var tabBarController: UITabBarController = {
         let tabBarController = UITabBarController()
-        tabBarController.setViewControllers([folderViewController, downloadViewController, moreSettingViewController], animated: false)
+        tabBarController.setViewControllers([folderViewController, downloadViewController], animated: false)
         
         if let items = tabBarController.tabBar.items{
             items[0].image = UIImage(named: "folder.fill")
             items[1].image = UIImage(named: "download.fill")
-            items[2].image = UIImage(named: "menu")
         }
         tabBarController.tabBar.backgroundColor = .white
         return tabBarController
