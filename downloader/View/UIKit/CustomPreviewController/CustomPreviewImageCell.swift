5f99258aabb40989a1b97defbfebf5aee325e8af
tanpmcluitk14 <tan.lk16.cla@gmail.com>
2022-08-30 18:25:54 +0700

    fix zoom out issue

--- downloader/View/UIKit/CustomPreviewController/CustomPreviewImageCell.swift
+++ downloader/View/UIKit/CustomPreviewController/CustomPreviewImageCell.swift
@@ -183,6 +183,12 @@ extension CustomPreviewImageCell: UIScrollViewDelegate{
         return imageView
     }
     
+    func centreView(scrollView: UIScrollView){
+        let offSetX = max((scrollView.bounds.size.width - scrollView.contentSize.width)*0.5, 0.0)
+        let offSetY = max((scrollView.bounds.size.height - scrollView.contentSize.height)*0.5, 0.0)
+        imageView.center = CGPoint(x: scrollView.contentSize.width*0.5+offSetX, y: scrollView.contentSize.height*0.5+offSetY)
+    }
+    
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
         if(scrollView.zoomScale == 1){
             // exit zoom mode
@@ -191,6 +197,8 @@ extension CustomPreviewImageCell: UIScrollViewDelegate{
             // enter zoom mode
             delegate?.setZoomState(isInZoomState: true)
         }
+        
+        centreView(scrollView: scrollView)
         
         if(scrollView.zoomScale >= 1){
             if let image = imageView.image{
